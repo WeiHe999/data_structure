3c374d4946b9a217f07e5ef0439c63df5ee65402
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-11-03 18:47:51 -0400

    Update utilities.cpp

--- utilities.cpp
+++ utilities.cpp
@@ -16,36 +16,16 @@ vector <string> split (string str1)
     return vv;
 }
 
-// 123 --> 123rd
-string number_to_ordinal(int number) {
-  string suffix = "th";
-  if (number % 100 < 11 || number % 100 > 13) {
-    switch (number % 10) {
-      case 1:
-        suffix = "st";
-        break;
-      case 2:
-        suffix = "nd";
-        break;
-      case 3:
-        suffix = "rd";
-        break;
-    }
-  }
-  return to_string(number) + suffix;
-}
-
 /*
 if the last 2 digits are 11, 12, or 13, use th
 else if the last 1 digit is 1, 2, or 3, use st, nd, rd;
 else use th
 */
-string number_to_ordinal_v2(int number) {
-    string suffix;
-    if (number%100==11 || number%100==12 || number%100==13) suffix = "th";
-    else if (number%10==1) suffix = "st";
-    else if (number%10==2) suffix = "nd";
-    else if (number%10==3) suffix = "rd";
-    else suffix = "th";
-    return to_string(number) + suffix;
+string number_to_ordinal_v2(int number)
+{
+    if (number % 100 == 11 || number % 100 == 12 || number % 100 == 13) return to_string(number) + "th";
+    else if (number % 10 == 1) return to_string(number) + "st";
+    else if (number % 10 == 2) return to_string(number) + "nd";
+    else if (number % 10 == 3) return to_string(number) + "rd";
+    else return to_string(number) + "th";
 }
