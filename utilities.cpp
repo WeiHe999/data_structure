6ceb39207b31626a1fde728965dc55bcf171b246
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-11-02 21:44:08 -0400

    Update utilities.cpp

--- utilities.cpp
+++ utilities.cpp
@@ -1,1 +1,36 @@
-hhhj
+#include <bits/stdc++.h>
+using namespace std;
+
+template < typename T > void print (T t)
+{
+    for (const auto & e:t) cout << e << " ";
+    cout << endl;
+}
+
+vector <string> split (string str1)
+{
+    vector <string> vv;
+    string word;
+    istringstream ss (str1);
+    while (getline (ss, word, ' ')) vv.emplace_back(word);
+    return vv;
+}
+
+// 123 --> 123rd
+string number_to_ordinal(int number) {
+  string suffix = "th";
+  if (number % 100 < 11 || number % 100 > 13) {
+    switch (number % 10) {
+      case 1:
+        suffix = "st";
+        break;
+      case 2:
+        suffix = "nd";
+        break;
+      case 3:
+        suffix = "rd";
+        break;
+    }
+  }
+  return to_string(number) + suffix;
+}
