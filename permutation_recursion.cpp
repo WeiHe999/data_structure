5f18ccdb8e71324714bff506af1f88b4518d7255
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2022-02-15 15:56:48 -0500

    Update permutation_recursion.cpp

--- permutation_recursion.cpp
+++ permutation_recursion.cpp
@@ -1,59 +1,37 @@
-// C++ program to print all
-// possible strings of length k
+/*
+permute to length-3 strings using only chars {'2', '3},
+result:
+222
+223
+232
+233
+322
+323
+332
+333
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
-	
 
-// The main recursive method
-// to print all possible
-// strings of length k
-
-
-void printAllKLengthRec(char set[], string prefix, int n, int k, int& tot, string g_str)
+void permute(vector<char> v1, string prefix, int tot_len)
 {
-	
-	// Base case: k is 0,
-	// print prefix
-	if (k == 0)
-	{
-		cout << (prefix) << endl;
-		if (prefix <= g_str) tot++;
-		return;
-	}
-
-	// One by one add all characters
-	// from set and recursively
-	// call for k equals to k-1
-	for (int i = 0; i < n; i++)
-	{
-		string newPrefix;
-		
-		// Next character of input added
-		newPrefix = prefix + set[i];
-		
-		// k is decreased, because
-		// we have added a new character
-		printAllKLengthRec(set, newPrefix, n, k - 1, tot, g_str);
-	}
-
+    if (prefix.size()==tot_len)
+    {
+        cout << prefix << endl;
+        return;
+    }
+    else
+    {
+        for (auto x: v1)
+        {
+            string new_prefix = prefix + x;
+            permute(v1, new_prefix, tot_len);
+        }
+    }
 }
 
-void printAllKLength(char set[], int k,int n, int & tot, string g_str)
-{
-	printAllKLengthRec(set, "", n, k, tot, g_str);
+int main() {
+    vector<char> v1 = {'2', '3'};
+    permute(v1, "", 3);
 }
-
-// Driver Code
-int main()
-{
-	
-	cout << "First Test" << endl;
-	char set1[] = {'2', '3'};
-	int k = 5;
-	int tot = 0;
-        string g_str = "33222";
-	printAllKLength(set1, k, 2, tot, g_str);
-	cout << tot << endl;
-	
-}
-
